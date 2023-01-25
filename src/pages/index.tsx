57810186d72bfe474fa49c40f09a5af7201f9437
Alex <AlexGrimes.se@gmail.com>
2023-01-25 18:43:06 -0500

    adding the spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,19 +15,27 @@ export default function Home() {
   let movies = [];
   let searchedMovies = [];
   const [searchInput, setsearchInput] = useState('')
+  const [isLoading, setisLoading] = useState(true)
 
   async function getMovies() {
+    setisLoading(true)
     const data = axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=332bbed1717d46c67174b3e563235770&language=en-US&page=1`);
     const result = await data;
+    if(result){
+      setisLoading(false);
+    }
     result.data.resultrs.forEach((movie: any) => {
       movies.push(movie);
     });
   }
 
   async function searchMovies() {
-    
+    setisLoading(true);
     const data = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=332bbed1717d46c67174b3e563235770&language=en-US&page=1&query=${searchInput}`);
     const result = await data;
+    if(result){
+      setisLoading(false);
+    }
     result.data.results.forEach((movie: any) => {
       searchedMovies.push(movie);
     })
@@ -45,9 +53,12 @@ export default function Home() {
     <div className={styles.home}>
       <Hero /> 
       <div className={`${styles.container} ${styles.search}`}>
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" value={searchInput}/>
         <button className='button' onClick={clearSearch}>Clear Search</button>
       </div>
+      { isLoading &&
+        <Loading />
+      }
     </div>
     </>
   )
