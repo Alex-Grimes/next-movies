ddeb72a1e164dd59ee9872a35e215990bb2bff9d
Alex <alexgrimes.se@gmail.com>
2023-05-07 18:02:20 -0400

    work on back button

--- src/pages/movies/[movieid].tsx
+++ src/pages/movies/[movieid].tsx
@@ -1,44 +1,58 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { Loading } from '@/components/Loading/Loading';
 import Link from 'next/link';
 import { useQuery } from 'react-query';
-import styles from '@/styles/Home.module.scss'
+import styles from '@/styles/Home.module.scss';
 import Image from 'next/image';
 
 const Movieid = () => {
+  const router = useRouter();
+  const { movieid } = router.query;
 
-  const router = useRouter()
-  const { movieid } = router.query
-
-  
   async function getSingleMovie() {
-    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${movieid}?api_key=332bbed1717d46c67174b3e563235770&language=en-US&page=1`);
-    return data
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieid}?api_key=332bbed1717d46c67174b3e563235770&language=en-US&page=1`
+    );
+    return data;
   }
 
-  function imageLoader({ src, width, quality}: any){
-    return `https://image.tmdb.org/t/p/w500/${src}?w=${width}&q=${quality || 75}`
+  function imageLoader({ src, width, quality }: any) {
+    return `https://image.tmdb.org/t/p/w500/${src}?w=${width}&q=${
+      quality || 75
+    }`;
   }
 
   function RenderMovies() {
-    const {data, isError, isLoading} = useQuery('movies', getSingleMovie)
-    if(isLoading){
-      return <Loading/>
+    const { data, isError, isLoading } = useQuery('movies', getSingleMovie);
+    if (isLoading) {
+      return <Loading />;
     }
-    if(isError){
-      return <div>Error!</div>
-  }
-  if(data){
+    if (isError) {
+      return <div>Error!</div>;
+    }
+    if (data) {
       return (
-      <div className={styles.movieimg}>       
-          <Image className={styles.imageWrapper} loader={imageLoader} src={data.poster_path} alt="Movie Poster" width={800} height={800}/>
-          <p className={styles.review}>{data.vote_average}</p>
-          <p className={styles.overview}>{data.overview}</p>
-      </div>
+        <>
+          <Link className={styles.button} href={'/'}>
+            Back
+          </Link>
+          <div className={styles.movieimg}>
+            <Image
+              className={styles.imageWrapper}
+              loader={imageLoader}
+              src={data.poster_path}
+              alt='Movie Poster'
+              width={800}
+              height={800}
+            />
+            <p className={styles.review}>{data.vote_average}</p>
+            <p className={styles.overview}>{data.overview}</p>
+          </div>
+        </>
 
-/* <div class="movie-info">
+        /* <div class="movie-info">
   <div class="movie-img">
     <img
       :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
@@ -76,16 +90,15 @@ const Movieid = () => {
   </div>
 </div>
 </div> */
-    
-    )}}
-  
+      );
+    }
+  }
 
   return (
     <div className={`${styles.container} ${styles.singlemovie}`}>
       {RenderMovies()}
-      <Link className={styles.button} href={'/'}>Back</Link>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Movieid
+export default Movieid;
